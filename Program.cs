64181993a9af3f4ff1dfcc2f4858407505dbeda5
Raphael Fadimu <raphaelfadimu@gmail.com>
2024-12-27 11:48:57 +0100

    [chore]: Set up packages

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+IDbConnection dbConnection = new SqlConnection(connectionString);
+
+string sqlCommand = "SELECT GETDATE()";
+
+DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+System.Console.WriteLine(rightNow);
